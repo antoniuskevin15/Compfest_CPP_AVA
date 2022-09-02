322e45d29db6fa22011ae726d183465ca5a8f66a
atras30 <atrasshalhan@gmail.com>
2022-09-02 14:07:43 +0700

    barisan bilangan

--- Pemrograman Kompetitif Dasar/Divide And Conquer/BarisanBilanganRevisited.cpp
+++ Pemrograman Kompetitif Dasar/Divide And Conquer/BarisanBilanganRevisited.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -6,16 +7,28 @@ int main() {
 
   cin >> n >> a >> b;
 
-  int counter = 0;
-  for(int i = a; 1; i++) {
-    if(i%a == 0 || i%b == 0) {
-      counter++;
-      if(counter == n) {
-        cout << i;
-        return 0;
-      }
-    }
+  list<int> sequenceA;
+
+  sequenceA.push_back(1);
+  sequenceA.push_back(2);
+  sequenceA.push_back(3);
+  sequenceA.push_back(4);
+
+  list<int>::iterator it;
+  for(it = sequenceA.begin(); it != sequenceA.end(); it++) {
+    cout << *it;
   }
+
+  // int counter = 0;
+  // for(int i = a; 1; i++) {
+  //   if(i%a == 0 || i%b == 0) {
+  //     counter++;
+  //     if(counter == n) {
+  //       cout << i;
+  //       return 0;
+  //     }
+  //   }
+  // }
 
 
   
