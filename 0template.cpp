002b21e13eab23be57232428591271f1ee61eaec
antoniuskevin15 <antoniuskevin15@gmail.com>
2022-09-04 11:01:18 +0700

    a dan B

--- 0template.cpp
+++ 0template.cpp
@@ -1,4 +1,18 @@
+// scpc_2_ava
 #include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <iomanip>
+#include <string>
+#include <math.h>
+#include <iterator>
+
+#define l long
+#define ll long long
+#define ld long double
+#define lld long long double
 
 using namespace std;
 
--- Penyisihan/AkumulasiDomino.cpp
+++ Penyisihan/AkumulasiDomino.cpp
@@ -1,11 +1,26 @@
 // scpc_2_ava
-
+#define ll long long
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-
+    ll n, m;
+    cin >> n >> m;
+    ll fill = 1;
+    if (n == 1 && m == 1)
+    {
+        fill = 0;
+    }
+    else if (n > m && m == 1)
+    {
+        fill = m * (n - 1);
+    }
+    else if (m > 1)
+    {
+        fill = (m - 1) * n;
+    }
+    cout << fill;
     return 0;
 }
